ae1c45f14ceac31190600500996ef7f127572ad0
Rylee Lyman <rylee.alanza@gmail.com>
2022-10-05 23:30:15 -0400

    still buggy

--- lib/mod.lua
+++ lib/mod.lua
@@ -7,26 +7,39 @@ local tNUMBER   = 1
 local param_options = {"none"}
 local watched = 0
 local formation = {}
+local exclude = {}
 
 mod.hook.register("script_pre_init", "choukanzu_script_pre_init", function()
     local script_init = init
     init = function()
+        for id, _ in pairs(params.lookup) do
+            table.insert(exclude, id)
+        end
         script_init()
-        for _, param in pairs(params.lookup) do
-            if param.t == tCONTROL or param.t == tTAPER or param.t == tNUMBER then
-                table.insert(param_options, param.id)
+        for id, _ in pairs(params.lookup) do
+            local param = params:lookup_param(id)
+            if param.t == tCONTROL or param.t == tTAPER then
+                local flag = true
+                for _, compare in pairs(exclude) do
+                    if id == compare then flag = false break end
+                end
+                if flag then
+                    table.insert(param_options, param.id)
+                end
             end
         end
+        params:add_separator("choukanzu", "~ choukanzu ~")
         params:add{
             type    = "control",
             id      = "choukanzu_fly",
             name    = "fly",
             action  = function(val)
                 local steps = #formation
-                if steps <= 0 then return end
-                local prev = math.floor(val * steps)
-                local next = math.ceil(val * steps)
-                for _, id in pairs(param_options) do
+                if steps <= 1 then return end
+                local prev = math.floor(util.linlin(0, 1, 1, steps, val))
+                local next = math.ceil(util.linlin(0, 1, 1, steps, val))
+                for i = 1, watched do
+                    local id = param_options[params:get("choukanzu_bird_" .. i)]
                     if id ~= "none" and formation[prev][id] and formation[next][id] then
                         local newval = util.linlin(prev, next, formation[prev][id], formation[next][id], val)
                         params:set(id, newval)
@@ -48,6 +61,7 @@ mod.hook.register("script_pre_init", "choukanzu_script_pre_init", function()
                     default = 1,
                     action  = function() formation = {} end
                 }
+                _menu.rebuild_params()
             end
         }
         params:add{
